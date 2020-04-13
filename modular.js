7a7c46980ca30b86a77fe977a5566fc81812fc6b
Colin Pace <colinpace0001@gmail.com>
2020-04-13 04:58:22 -0500

    organization

---
+++ modular.js
@@ -0,0 +1,201 @@
+const Container = function() {
+
+  // Variables
+  let form = document.getElementById('inputForm');
+
+  let enter = document.getElementById('enter');
+
+  let nameInput = document.getElementById('name');
+
+  let name = document.getElementById('name').value;
+
+  let retrieveButton = document.getElementById('retrieve');
+
+  let clear = document.getElementById('clear');
+
+  let list = document.getElementById('name-list');
+
+
+  // Methods
+  function sendItem(name) {
+
+    console.log(name);
+
+    let postBody = { 'item': name };
+
+    const options = {
+
+      method: 'POST',
+
+      body: JSON.stringify(postBody),
+
+      headers: {
+
+          "Content-Type": "application/json"
+
+        }
+    };
+
+    fetch('http://localhost:8000/', options)
+
+      .then( response => {
+
+        if (response.ok) {
+
+          console.log(response);
+
+        } else {
+
+          throw new Error('An error occured.')
+
+        }
+
+      })
+
+      .catch( error => {
+
+        console.log(error);
+
+      })
+  }
+
+  function retrieve() {
+
+    const options = {
+
+      method: 'GET',
+
+      headers: {
+
+          "Content-Type": "application/json"
+
+        }
+    };
+
+    fetch('http://localhost:8000/', options)
+
+      .then( response => {
+
+        if (response.ok) {
+
+          return response.json();
+
+        } else {
+
+          throw new Error('An error occured');
+
+        }
+
+      })
+
+      .then( data => {
+
+        display(data);
+
+      })
+
+      .catch( error => {
+
+        console.log(error);
+
+      })
+  }
+
+  function display(data) {
+
+    list.style.display = "block";
+
+    while (list.hasChildNodes()) {
+
+      list.removeChild(list.firstChild);
+
+    }
+
+    for (let i = 0; i < data.length; i++) {
+
+      let node = document.createElement('LI');
+
+      let textnode = document.createTextNode(`${data[i]}`);
+
+      node.appendChild(textnode);
+
+      list.appendChild(node);
+
+    }
+  }
+
+  function clearList() {
+
+    const options = {
+
+      method: 'DELETE',
+
+      headers: {
+
+          "Content-Type": "application/json"
+
+        }
+    };
+
+    fetch('http://localhost:8000/del/', options)
+
+      .then( response => {
+
+        if (!response.ok) {
+
+          throw new Error('An error occured.');
+
+        } else {
+
+          console.log(response);
+
+        }
+      })
+
+      .catch( error => {
+
+        console.log(error);
+
+      })
+  }
+
+
+  // Initialization and event listeners
+  enter.addEventListener('click', function(e) {
+
+    list.style.display = "none";
+
+    nameInput.style.display = "block";
+
+  });
+
+  form.addEventListener('submit', function(e) {
+
+    e.preventDefault();
+
+    name = document.getElementById('name').value;
+
+    sendItem(name);
+
+    document.getElementById('name').value = '';
+
+  });
+
+  retrieveButton.addEventListener('click', function(e) {
+
+    nameInput.style.display = "none";
+
+    retrieve();
+
+  });
+
+  clear.addEventListener('click', function(e) {
+
+    list.style.display = "none";
+
+    clearList();
+
+  })
+}
+
+Container();
