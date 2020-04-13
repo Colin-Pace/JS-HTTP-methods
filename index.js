8433c0cd6ec77d6f600aa5dfe9afea3ef7b35200
Colin Pace <colinpace0001@gmail.com>
2020-04-13 01:50:26 -0500

    del functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Show input
 document.getElementById('enter').addEventListener('click', function(e) {
 
+  document.getElementById('name-list').style.display = "none";
+
   document.getElementById('name').style.display = "block";
 
 });
@@ -62,7 +64,6 @@ document.getElementById('retrieve').addEventListener('click', function(e) {
 
 });
 
-
 function retrieve() {
 
   const options = {
@@ -94,9 +95,15 @@ function retrieve() {
 
 function display(data) {
 
-  // console.log(data);
-
   document.getElementById('name-list').style.display = "block";
+
+  let list = document.getElementById('name-list');
+
+  while (list.hasChildNodes()) {
+
+    list.removeChild(list.firstChild);
+
+  }
 
   for (let i = 0; i < data.length; i++) {
 
@@ -108,8 +115,39 @@ function display(data) {
 
     document.getElementById('name-list').appendChild(node);
 
-    // console.log(node);
-
   }
-
 }
+
+
+// Clear
+document.getElementById('clear').addEventListener('click', function(e) {
+
+  document.getElementById('name-list').style.display = "none";
+
+  const options = {
+
+    method: 'DELETE',
+
+    headers: {
+
+        "Content-Type": "application/json"
+
+      }
+
+  };
+
+  fetch('http://localhost:8000/del/', options)
+
+    .then((response) => {
+
+      return response.json();
+
+    })
+
+    .then((data) => {
+
+      display(data);
+
+    });
+
+})
